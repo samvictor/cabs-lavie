f780460a1409ca20521ae0e8fe6fbc4715d9995c
Markous Soliman <markous.soliman@gmail.com>
2018-10-14 23:43:38 -0400

    #12: added temporary spinner to page

--- app/ui/src/components/App.tsx
+++ app/ui/src/components/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import autoBind from 'auto-bind';
 
 interface IProps {
   loadingState: number
@@ -14,19 +15,32 @@ type IAllProps = IProps & IDispatchProps;
 class App extends React.Component<IAllProps,any> {
   constructor(props){
     super(props);
+    autoBind(this);
   }
 
   private incrementNumber(){
     this.props.incrementLoadingState();
   };
+
+  renderSpinner(){
+    return <div className={"lds-facebook"}>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+  }
   
   render() {
     const {loadingState} = this.props;
+    const spinner = loadingState < 5 && loadingState > 0? this.renderSpinner(): '';
 
     return (
-      <div onClick={() => this.incrementNumber()} >	im her 
-        <div>{loadingState}</div>
+      <div className="app">
+        <div className="bordered-button" onClick={this.incrementNumber}> Click me!</div>
+        <div className="loading-counter">{loadingState}</div>
+        {spinner}  
       </div>
+
     );
   }
 }
