ff5fd87ce1f586c92e255b1d8672238e52373676
sam <sjinniss1@gmail.com>
2018-10-18 14:17:52 -0400

    showing example of how to use firestore #22

--- app/ui/src/components/App.tsx
+++ app/ui/src/components/App.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import autoBind from 'auto-bind';
+import * as firebase from 'firebase';
+
 
 interface IProps {
   loadingState: number
@@ -17,7 +19,67 @@ class App extends React.Component<IAllProps,any> {
   constructor(props){
     super(props);
     autoBind(this);
+    
+    var config = {
+      apiKey: "AIzaSyARw_MQFNy0G_E6ng_oKVBCOeC1HrBNuTo",
+      authDomain: "cabs-lavie.firebaseapp.com",
+      databaseURL: "https://cabs-lavie.firebaseio.com",
+      projectId: "cabs-lavie",
+      storageBucket: "cabs-lavie.appspot.com",
+      messagingSenderId: "787170972614"
+    };
+    firebase.initializeApp(config);
+    
+    
+    this.state = {
+      'data_loading': true,
+      'data': {},
+    }
   }
+  
+  componentDidMount() {
+    let db = firebase.firestore();
+    const settings = {/* your settings... */ timestampsInSnapshots: true};
+    db.settings(settings);
+    
+    //let page = window.location.pathname.split('/')[2].toLowerCase().replace(/\W/g, '');
+    //if (typeof page === 'undefined' || page === '')
+    //  page = 'home';
+
+    let items_ref = db.collection('inventory').where('key', '>=', 'item1');
+    
+    items_ref.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+            
+            doc.data();
+            
+            let temp_data = this.state.data;
+            temp_data[doc.data().key] = doc.data();
+            
+            this.setState({'data': temp_data});
+            
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+            
+            //this.setState({'data': null, 'data_loading': false});
+        }
+        });
+        
+        this.setState({'data_loading': false});
+        //.catch((error) => {
+         //   console.log("Error getting document:", error);
+            //$('#alert_danger').text('Error').fadeIn().delay(2000).fadeOut();
+            
+         //   this.setState({'data': null, 'data_loading': false});    
+        //});
+      });
+
+  }
+
+    
 
   private incrementNumber(){
     this.props.incrementTwice();
@@ -34,13 +96,22 @@ class App extends React.Component<IAllProps,any> {
   render() {
     const {loadingState} = this.props;
     const spinner = loadingState < 5 && loadingState > 0? this.renderSpinner(): '';
-
+    
+    let for_xml = 'loading...';
+    
+    if (!this.state.data_loading) {
+      for_xml = JSON.stringify(this.state.data, null, 2);
+    }
+    
     return (
       <div className="app">
         <div className="bordered-button" onClick={this.incrementNumber}> Click me!</div>
         <div className="loading-counter">{loadingState}</div>
         {spinner}  
         It works for sam again!
+        <p style={{'whiteSpace': 'pre', 'overflowX': 'scroll'}}>
+          {for_xml}
+        </p>
       </div>
 
     );
